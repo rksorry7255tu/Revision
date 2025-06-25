2ceb476084105267b868e6956da02a405f35aecb
rksorry7255tu <rk7255053708@gmail.com>
2025-06-25 07:25:46 +0530

    commited

---
+++ Arrays/array1.java
@@ -0,0 +1,9 @@
+class array1 {
+  public static void main(String[] args) {
+    int arr[] = { 1, 2, 3, 4, 5, 6 };
+    for (int i = 0; i < arr.length; i++) {
+      System.out.println(arr[i]);
+    }
+  }
+}
+// C:\Program Files\Java\jdk1.8.0_112\bin
---
+++ Arrays/convert.java
@@ -0,0 +1,14 @@
+public class convert {
+  public static void main(String[] args) {
+    // integer to string
+    int a = 20;
+    String b = String.valueOf(a);
+    System.out.println(b);
+
+    // string to integer
+    String c = "20";
+    int d = Integer.valueOf(c);
+    System.out.println(d);
+
+  }
+}
